22ca19d29d3995b022a78048fe020fd4af970814
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2020-09-06 18:35:21 +0530

    genesis

---
+++ src/test/java/com/isumalab/antlr/AntlrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isumalab.antlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntlrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
